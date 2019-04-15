c4e21bf70b25bf690d7c79e3f80b4f3095cabb2c
Emirhan Akman <emirhan_0607@hotmail.com>
2019-04-15 12:34:28 +0300

    Update dortgen_alan_hesapla_v1.py

--- dortgen_alan_hesapla_v1.py
+++ dortgen_alan_hesapla_v1.py
@@ -2,6 +2,6 @@ def dortgen_alan_hesapla_v1(genislik, yukseklik):
     alani = int(genislik) * int(yukseklik) #genişlik ile yükseklik değerini çarpıyoruz // kısa kenar ile uzun kenar da denilebilir.
     print ("Alan :", alani ,"m2") #burada alan= alan değeri ve sonuna da m2 ifadesi ekledim.
     return alani
-genislik_degeri = input("Genişlik: ")
-yukseklik_degeri = input("Yükseklik: ")
+genislik_degeri = input("Genişlik: ") #genislik degerini kullanicidan aliyoruz
+yukseklik_degeri = input("Yükseklik: ") #yukseklik degerini kullanicidan alıyoruz
 dortgen_alan_hesapla_v1(genislik_degeri, yukseklik_degeri)
